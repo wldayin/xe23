aeef157f392141ee0e003d4f82b2756077c4fd12
wldayin <23wldayin@163.com>
2013-08-21 17:41:19 +0800

    Revert "init files"
    This reverts commit fd821aacf0a07e75123807cf089a8e55b2f93b94.

--- wp-content/themes/easy-view/searchform.php
+++
@@ -1,4 +0,0 @@
-<form method="get" action="<?php bloginfo('url'); ?>/" >
-			<input type="text" id="s" value="" name="s" class="field" />
-			<input type="submit" id="submit" value="Search!" />
-		</form>
--- wp-content/themes/jiudianplus/404.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-
-<div class="archive">
-<h1 class="center">Error 404 - Not Found</h1>
-</div>
-
-<?php get_sidebar(); ?>
-</div>
-<?php get_footer(); ?>
